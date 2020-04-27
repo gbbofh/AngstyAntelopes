40738f0152913c14bc9625984bcb7b5a80d7d27a
shavonne-masters <52932884+shavonne-masters@users.noreply.github.com>
2020-04-26 23:16:26 -0600

    Update LevelManager.cs
    added comments

--- UntitledProject/Assets/Scripts/Core/Managers/LevelManager.cs
+++ UntitledProject/Assets/Scripts/Core/Managers/LevelManager.cs
@@ -30,6 +30,7 @@ namespace Core.Managers
         public void LoadLevel(string levelName, bool activateOnLoad = true, bool loadAdditive = false) {
 
             gameManager.OnLoadBegin();
+            //this makes load from scene function run cuncurently with everything else
             StartCoroutine(LoadFromScene(levelName, activateOnLoad, loadAdditive));
         }
 
@@ -58,7 +59,8 @@ namespace Core.Managers
                 loadOp.allowSceneActivation = true;
             }
         }
-
+        
+        //this tells you how long your waiting
         public float GetLoadProgress() {
 
             return Mathf.Clamp((loadOp == null) ? 1.0f : (loadOp.progress / 0.9f), 0.0f, 1.0f);
