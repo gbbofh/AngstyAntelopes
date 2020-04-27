eec620831c932f3141af5c2b26deb070fa5bb198
shavonne-masters <52932884+shavonne-masters@users.noreply.github.com>
2020-04-26 23:27:13 -0600

    Update GameManager.cs
    added comments

--- UntitledProject/Assets/Scripts/Core/Managers/GameManager.cs
+++ UntitledProject/Assets/Scripts/Core/Managers/GameManager.cs
@@ -13,11 +13,17 @@ namespace Core.Managers
 
         private LevelManager levelManager;
 
+        //called whenever the GameManager is created
+        //called before anything else when a script 
+        //  is added to an object
         private void Awake() {
 
             levelManager = LevelManager.Instance;
         }
-
+        
+        //called after all of the other script have had 
+        //  their awake method envoked
+        //config.txt: gives deginitions for physical actions
         private void Start() {
 
             conf = Config.Reader.ReadFile(Application.dataPath + "\\config.txt");
@@ -27,7 +33,9 @@ namespace Core.Managers
                 onConfigLoad();
             }
         }
-
+        
+        //this default values in case the config.txt is 
+        //  empty/does not exist
         private void OnDestroy() {
 
             if (conf.Changed) {
@@ -35,14 +43,22 @@ namespace Core.Managers
                 Config.Writer.WriteFile(Application.dataPath + "\\config.txt", conf);
             }
         }
-
+        
+        //ToDo: when the LevelManager starts loading a scene, 
+        //  OnLoadBegin is called so that the GameManager can 
+        //  do whatever setup it needs to do for level (things 
+        //  like cleaning up the last scene or activing the 
+        //  level loading UI)
         public void OnLoadBegin() {
         }
-
+        
+        //ToDo: called when the LevelManager ends loading a 
+        //  scene, this method will activate the scene from 
+        //  LoadManager.
         public void OnLoadEnd() {
 
         }
-
+        
         public void OnPlayerDead() {
 
             levelManager.LoadLevel("GameOver", true, false);
