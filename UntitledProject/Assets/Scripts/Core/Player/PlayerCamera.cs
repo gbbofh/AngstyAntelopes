8a718658a8a94ec1b93adf3ae0d3d9e31161f4bc
shavonne-masters <52932884+shavonne-masters@users.noreply.github.com>
2020-04-26 23:33:54 -0600

    Update PlayerCamera.cs
    added comments

--- UntitledProject/Assets/Scripts/Core/Player/PlayerCamera.cs
+++ UntitledProject/Assets/Scripts/Core/Player/PlayerCamera.cs
@@ -7,15 +7,21 @@ namespace Core.Player
 {
     public class PlayerCamera : MonoBehaviour
     {
+        // ToDo: this happens after getting input from mouse
+        //   now we apply it to the camera
         private void OnInput(Vector2 mouseDelta) {
 
         }
 
         // Update is called once per frame
+        // ToDo: where the actual updating 
+        //   of the camera movement
         void Update() {
 
         }
-
+        
+        // Listining to whenever Unity callback "onLook"
+        //  is invoked
         public void Connect(PlayerInput input) {
 
             input.onLook += OnInput;
