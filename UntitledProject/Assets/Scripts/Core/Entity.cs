3fcc6c4bfd99db7cd448fa4d78aace501363fb74
shavonne-masters <52932884+shavonne-masters@users.noreply.github.com>
2020-04-26 23:49:00 -0600

    Update Entity.cs
    added comments

--- UntitledProject/Assets/Scripts/Core/Entity.cs
+++ UntitledProject/Assets/Scripts/Core/Entity.cs
@@ -12,8 +12,10 @@ namespace Core
         private EntityManager entityManager;
 
         // Start is called before the first frame update
+        // all entities will have this class on it
+        //      entity = (anything in the game that can move around).
         void Start() {
-
+            
             entityManager = EntityManager.Instance;
 
             entityManager.AddEntity(this);
